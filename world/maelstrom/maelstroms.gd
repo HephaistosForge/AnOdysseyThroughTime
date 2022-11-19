f9685955147d714094289b3938dca7d187ec66be
MarianZuska <marian.zuska@uni-rostock.de>
2022-11-19 23:37:57 +0100

    make maelstroms spawn randomly again

--- world/maelstrom/maelstroms.gd
+++ world/maelstrom/maelstroms.gd
@@ -5,7 +5,9 @@ const AREA_PREFAB: PackedScene = preload("res://world/maelstrom/maelstrom_area.t
 @export var maelstroms: Array[Vector3]
 
 var spawn_point_as_uv = Vector3(0.5, 0.5, 0)
-var mesh_size = 1200
+var mesh_size = 1800
+
+var ship
 
 func uv_to_world(pos):
 	return Vector3(pos.x-0.5, 0, pos.y-.5) * mesh_size
@@ -14,18 +16,30 @@ func world_to_uv(pos):
 	return Vector3(pos.x/mesh_size + 0.5, pos.z/mesh_size + 0.5, 0.2)
 
 func _ready():
-	for maelstrom in $Maelstroms.get_children():
-		maelstroms.append(world_to_uv(maelstrom.position))
-	#var pos;
-	#for i in 10:
-	#	while true:
-	#		pos = Vector3(randf(), randf(), .2)
-	#		if pos.distance_to(spawn_point_as_uv) > .3:
-	#			break
-	for i in len(maelstroms):
-		mesh.material.set_shader_parameter("maelstrom_positions" + str(i), maelstroms[i])
-		mesh_size = mesh.size.x
+	
+	mesh_size = mesh.size.x
+	if Globals.start_pos != Vector3.ZERO:
+		spawn_point_as_uv = world_to_uv(Globals.start_pos)
 		
+	#for maelstrom in $Maelstroms.get_children():
+	#	maelstroms.append(world_to_uv(maelstrom.position))
+	
+	var pos;
+	for i in 45:
+		var trials = 0
+		while true:
+			pos = Vector3(randf(), randf(), .2)
+			if pos.distance_to(spawn_point_as_uv) > 0.1 and pos.length() > 0.17:
+				if trials < 10:
+					for p in maelstroms:
+						if pos.distance_to(p) < 0.1:
+							trials += 1
+							continue
+				break
+		maelstroms.append(pos)
+		
+	for i in len(maelstroms):
+		#mesh.material.set_shader_parameter("maelstrom_positions" + str(i), maelstroms[i])
 		var area = AREA_PREFAB.instantiate()
 		add_child(area)
 		area.position = uv_to_world(maelstroms[i])
@@ -35,6 +49,22 @@ var time = 0
 func _process(delta):
 	# shader_mat.set_uniform_value("time", time)
 	time += delta
+	
+	var best = []
+	
+	ship = get_tree().get_first_node_in_group("boat")
+	var pos = ship.position
+	for m in maelstroms:
+		var dist = pos-uv_to_world(m)
+		dist = dist.length()
+		best.append([dist, m])
+	
+	best.sort()
+	best = best.slice(0,15)
+	
+	for i in len(best):
+		mesh.material.set_shader_parameter("maelstrom_positions" + str(i), best[i][1])
+		
 		
 func height(uv):
 	var h = sin(time * 1.0 + uv.x*100. + uv.y*120.)
