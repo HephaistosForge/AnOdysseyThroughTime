205a5d2bab912782d27b0fd2b2ae4d764f7afee2
MarianZuska <marian.zuska@uni-rostock.de>
2022-11-20 13:38:16 +0100

    make ship spawn scene initial after menu

--- ui/menu/menu.gd
+++ ui/menu/menu.gd
@@ -1,7 +1,7 @@
 extends PanelContainer
 
 func _on_play_pressed():
-	assert(get_tree().change_scene_to_file("res://world/ocean/ocean.tscn") == 0)
+	assert(get_tree().change_scene_to_file("res://world/ocean/ocean_spawning.tscn") == 0)
 	
 func _on_highscore_pressed():
 	assert(get_tree().change_scene_to_file("res://ui/highscore/highscore.tscn") == 0)
