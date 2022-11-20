64eee6ef6792c603027d068e648385e5692a2d55
Robert <robert-github@posteo.de>
2022-11-20 13:16:28 +0100

    Enable full audio volume mute

--- ui/options/options.gd
+++ ui/options/options.gd
@@ -5,3 +5,13 @@ func _on_steuerung_pressed():
 
 func _on_back_button_pressed():
 	assert(get_tree().change_scene_to_file("res://ui/menu/menu.tscn") == 0)
+	
+	if AudioServer.get_bus_volume_db(1) <= -41:
+		AudioServer.set_bus_mute(1, true)
+	else:
+		AudioServer.set_bus_mute(1, false)
+	
+	if AudioServer.get_bus_volume_db(2) <= -41:
+		AudioServer.set_bus_mute(2, true)
+	else:
+		AudioServer.set_bus_mute(2, false)
